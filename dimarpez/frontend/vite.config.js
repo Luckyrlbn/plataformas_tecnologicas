6076f18ddf650fd61d192ae6d8a57b724ebb986e
Lucky <jsantiagogelvesb@gmail.com>
2026-06-17 20:15:23 -0500

    Update vite.config.js

--- dimarpez/frontend/vite.config.js
+++ dimarpez/frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'plataformastecnologicas-production.up.railway.app',
+        target: 'http://localhost:8080',
         changeOrigin: true,
       }
     }
